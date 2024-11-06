d10937b4e8d240589d4163b921019665e75f4b46
circular <circular@circulars.dev>
2024-11-06 14:06:50 +1100

    make buttons more "bouncy"

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
       fontFamily: {
-        display: 'Rubik, ui-serif'
-      }
+        display: "Rubik, ui-serif",
+      },
+      transitionTimingFunction: {
+        bounce: "cubic-bezier(.33,1.11,.69,1.5)",
+      },
     },
-	},
-	plugins: [
+  },
+  plugins: [
     require("@catppuccin/tailwindcss")({
       prefix: "ctp",
       defaultFlavour: "mocha",
     }),
   ],
-}
+};
